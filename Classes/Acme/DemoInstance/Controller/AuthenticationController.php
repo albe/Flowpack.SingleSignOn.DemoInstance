9d5bc54ffa00bc3546f4b13f67420de802a1d347
Christopher Hlubek <hlubek@networkteam.com>
2012-11-27 17:20:40 +0100

    [TASK] Use bootstrap for DemoInstance

---
+++ Classes/Acme/DemoInstance/Controller/AuthenticationController.php
@@ -0,0 +1,37 @@
+<?php
+namespace Acme\DemoInstance\Controller;
+
+/*                                                                        *
+ * This script belongs to the Flow package "MyCompany.SingleSignOnDemo". *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Authentication controller for the SSO DemoInstance package
+ *
+ * @Flow\Scope("singleton")
+ */
+class AuthenticationController extends \TYPO3\Flow\Security\Authentication\Controller\AbstractAuthenticationController {
+
+	/**
+	 * Is called if authentication was successful. If there has been an
+	 * intercepted request due to security restrictions, you might want to use
+	 * something like the following code to restart the originally intercepted
+	 * request:
+	 *
+	 * if ($originalRequest !== NULL) {
+	 *     $this->redirectToRequest($originalRequest);
+	 * }
+	 * $this->redirect('someDefaultActionAfterLogin');
+	 *
+	 * @param \TYPO3\Flow\Mvc\ActionRequest $originalRequest The request that was intercepted by the security framework, NULL if there was none
+	 * @return string
+	 */
+	protected function onAuthenticationSuccess(\TYPO3\Flow\Mvc\ActionRequest $originalRequest = NULL) {
+		$this->redirect('index', 'Standard');
+	}
+
+}
+?>
