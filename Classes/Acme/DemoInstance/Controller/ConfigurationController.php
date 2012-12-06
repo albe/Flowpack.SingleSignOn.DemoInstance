8005da63d67d02af1f209327bc415764aed797cb
Christopher Hlubek <hlubek@networkteam.com>
2012-12-06 18:13:43 +0100

    [TASK] Improve demo client UI, add configuration overview

---
+++ Classes/Acme/DemoInstance/Controller/ConfigurationController.php
@@ -0,0 +1,45 @@
+<?php
+namespace Acme\DemoInstance\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Acme.DemoServer".       *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * SSO server configuration controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class ConfigurationController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\SingleSignOn\Client\Domain\Repository\SsoServerRepository
+	 */
+	protected $ssoClientRepository;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Configuration\ConfigurationManager
+	 */
+	protected $configurationManager;
+
+	/**
+	 * Display configuration of client
+	 */
+	public function indexAction() {
+		$clientSettings = $this->configurationManager->getConfiguration(\TYPO3\Flow\Configuration\ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'TYPO3.SingleSignOn.Client');
+		$clientSettingsYaml = \Symfony\Component\Yaml\Yaml::dump($clientSettings, 99, 2);
+		$this->view->assign('clientSettings', $clientSettingsYaml);
+
+		$authenticationSettings = $this->configurationManager->getConfiguration(\TYPO3\Flow\Configuration\ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'TYPO3.Flow.security.authentication');
+		$authenticationSettingsYaml = \Symfony\Component\Yaml\Yaml::dump($authenticationSettings, 99, 2);
+		$this->view->assign('authenticationSettings', $authenticationSettingsYaml);
+	}
+
+}
+
+?>
